ce06c81925b920ca164049bc6dec6f6464b8c539
sung.phan.hd <sung.phan.hdwesoft.dev>
2026-01-07 10:44:57 +0700

    chore: Inital project Blog platform with react framework

---
+++ blog_flatform/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button variant="outline">Welcome to Blog Platform</Button>
+    </div>
+  )
+}
+
+export default App
